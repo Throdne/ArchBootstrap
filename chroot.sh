9927c35b819916937514f9c5f90e45b755420721
Jerico Thomas <Throdne@gmail.com>
2024-02-01 16:58:58 -0700

    fixed pacman

--- chroot.sh
+++ chroot.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-echo "Inside Arch Linux bootstrap environment..."
+
+rootFS="$1"; shift
+systemd-machine-id-setup
 
 pacman-key --init
 pacman-key --populate
+pacman -Syu --needed --noconfirm "$@"&& printf '\e[32m>\e[m: %s\n' 'Updated & installed packages'
 
-exit 0
+printf '\e[32m>\e[m: %s\n%s\n' "Done. Arch installer environment setup; Chaged root into $rootFS" \
+  'You may now proceed to: https://wiki.archlinux.org/title/Installation_guide#Partition_the_disks and follow the rest of the installation guide'
+
+printf '%s\n' 'echo "Welcome to Arch Linux"' >>/etc/profile
+
+rm "$0"
+
+bash --login
