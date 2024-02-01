348f854ce01324998fdaa8318f8928cb76d370e6
Jerico Thomas <Throdne@gmail.com>
2024-02-01 16:34:12 -0700

    launch chroot.sh once inside chroot

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script to install Arch Linux on a VPS with out the option to boot via an ISO.
 # Download public key
@@ -14,9 +14,10 @@ tar xzf /tmp/archlinux-bootstrap-x86_64.tar.gz --numeric-owner
 # Sets the mirror for pacman 
 sed -i 's|#Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch|Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch|' /tmp/root.x86_64/etc/pacman.d/mirrorlist
 
-# Going int Arch Linux to complete the install
-/tmp/root.x86_64/bin/arch-chroot /tmp/root.x86_64/
+# download chroo.sh
+wget https://raw.githubusercontent.com/Throdne/ArchBootstrap/main/chroot.sh -O /tmp/root.x86_64/root/chroot.sh
+chmod +x /tmp/root.x86_64/root/chroot.sh
 
-# rebuilds pacman
-pacman-key --init
-pacman-key --populate
+# Going int Arch Linux to complete the install
+/tmp/root.x86_64/bin/arch-chroot /tmp/root.x86_64/ /root/chroot.sh
+
